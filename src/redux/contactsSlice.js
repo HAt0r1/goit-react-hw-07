d7fbbd4776f162a63e0078af345dc0fb91ce38c6
Hatori <ahgangadm@gmail.com>
2024-05-09 22:06:44 +0300

    Update contact slice

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchContacts, addContact, deleteContact } from "./operations";
+
+const setLoading = (state) => {
+  state.loading = true;
+};
+const setError = (state, action) => {
+  state.loading = false;
+  state.error = action.payload;
+};
+
+const contactSlice = createSlice({
+  name: "contacts",
+  initialState: {
+    items: [],
+    loading: false,
+    error: null,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchContacts.pending, setLoading)
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.items = action.payload;
+      })
+      .addCase(fetchContacts.rejected, setError)
+      .addCase(addContact.pending, setLoading)
+      .addCase(addContact.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.items.push(action.payload);
+      })
+      .addCase(addContact.rejected, setError)
+      .addCase(deleteContact.pending, setLoading)
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.items.filter((contact) => contact.id !== action.payload);
+      })
+      .addCase(deleteContact.rejected, setError);
+  },
+});
+
+export default contactSlice.reducer;
